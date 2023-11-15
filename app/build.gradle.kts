9640e6e4c89a006699c36365b8ca64934fd1ddce
rborrozino <rafael.borrozino@venturus.org.br>
2023-11-14 22:42:24 -0300

    add home screen and home viewmodel classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
 
     //dagger hilt
     implementation ("com.google.dagger:hilt-android:2.48.1")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
 
     //retrofit
